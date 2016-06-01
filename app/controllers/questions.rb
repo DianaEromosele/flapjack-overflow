1a0d6138bb178db41390f9a0390782bc86ceb60d
George Skrapits <email": null>
2016-06-01 13:01:46 -0400

    complete questions CRUD

---
+++ app/controllers/questions.rb
@@ -0,0 +1,58 @@
+get '/questions' do
+  @qs = Question.all
+  erb :'questions/index'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  @q = Question.new(title: params['title'], body: params['body'], user_id: session['user_id'])
+  if @q.save
+    redirect '/questions'
+  else
+    @errors = @q.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
+get '/questions/:id' do
+  @q = Question.find_by(id: params[:id])
+  halt(404, erb(:'404')) if @q.nil?
+  erb :"questions/show"
+end
+
+get '/questions/:id/edit' do
+  @q = Question.find_by(id: params[:id])
+  # if session["user_id"] == @entry.user_id
+    erb :'questions/edit'
+  # else
+  #   erb :'404'
+  # end
+end
+
+put '/questions/:id' do
+  @q = Question.find_by(id: params[:id])
+  # if session["user_id"] == @q.user_id
+    @q.assign_attributes(title: params['title'], body: params['body'])
+    if @q.save
+      redirect "questions/#{@q.id}"
+    else
+      @errors = @q.errors.full_messages
+      erb :'questions/edit'
+    end
+  # else
+  #   erb :'404'
+  # end
+end
+
+delete '/questions/:id' do
+  @q = Question.find_by(id: params[:id])
+  # if session["user_id"] == @q.user_id
+    @q.destroy
+    redirect '/questions'
+  # else
+  #   erb :'404'
+  # end
+end
