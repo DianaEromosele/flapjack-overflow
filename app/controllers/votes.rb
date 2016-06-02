7ed4be38c3beba0a75b0965679947188b4b66699
George Skrapits <email": null>
2016-06-02 16:28:12 -0400

    destroy upvote only if it exists

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -6,20 +6,20 @@ end
 
 post '/questions/:id/downvote' do
   question = Question.find_by(id: params[:id])
-  question.votes.find_by(user_id: session[:user_id]).destroy
+  vote = question.votes.find_by(user_id: session[:user_id])
+  vote.destroy if vote
   redirect "/questions/#{question.id}"
 end
 
 post '/answers/:id/upvote' do
-  question = Question.find_by(id: params[:id])
-  answer = question.answers.find_by(id: params[:id])
+  answer = Answer.find_by(id: params[:id])
   answer.votes.create(user_id: session['user_id'])
   redirect "/question/#{question.id}"
 end
 
 post '/answers/:id/downvote' do
-  question = Question.find_by(id: params[:id])
-  answer = question.answers.find_by(id: params[:id])
-  answer.votes.find_by(user_id: session[:user_id]).destroy
+  answer = Answer.find_by(id: params[:id])
+  vote answer.votes.find_by(user_id: session[:user_id])
+  vote.destroy if vote
   redirect "/questions/#{question.id}"
 end
